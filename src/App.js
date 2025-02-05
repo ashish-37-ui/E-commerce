a849ae1364d3fe56c5f1fc95f3a3a45a8f70bb1b
ashish-me-15 <ashish.sharma75.email@gmail.com>
2025-02-06 04:49:48 +0530

    added routes of the web page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignUp from './Pages/LoginSignUp';
 
 function App() {
   return (
     <div>
-    <Navbar />
+      <BrowserRouter>
+      
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/Men' element={<ShopCategory category="men"/>}/>
+        <Route path='/Women' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+
+        <Route path='/product' element={<Product/>}>
+
+        <Route path=':productId' element={<Product/>}/>
+
+        </Route>
+
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignUp/>}/>
+
+
+
+
+
+      </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
